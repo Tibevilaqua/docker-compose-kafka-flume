8d6e3e4cf4bbdda4c033b594836e499462054ffe
tbevilaqua <tbevilaqua@shelde.com>
2019-03-08 13:37:49 +1100

    Adding another flume to consume from kafka and display messages to terminal.Upgrading flume's version from 1.6.0 to 1.9.0

--- scripts/set-up.sh
+++ scripts/set-up.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-curl -L http://www-eu.apache.org/dist/flume/1.6.0/apache-flume-1.6.0-bin.tar.gz -o apache-flume-1.6.0-bin.tar.gz
+curl -L http://www-eu.apache.org/dist/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz -o apache-flume-1.9.0-bin.tar.gz
 curl -L https://www.apache.org/dist/flume/KEYS -o KEYS
-curl -L https://www.apache.org/dist/flume/1.6.0/apache-flume-1.6.0-bin.tar.gz.asc -o apache-flume-1.6.0-bin.tar.gz.asc
-curl -L https://www.apache.org/dist/flume/1.6.0/apache-flume-1.6.0-bin.tar.gz.md5 -o apache-flume-1.6.0-bin.tar.gz.md5
 
-mv apache-flume-1.6.0-bin.tar.gz ../flume/files/
-mv KEYS ../flume/files/
-mv apache-flume-1.6.0-bin.tar.gz.asc ../flume/files/
-mv apache-flume-1.6.0-bin.tar.gz.md5 ../flume/files/
+cp apache-flume-1.9.0-bin.tar.gz ../flume/files/
+cp KEYS ../flume/files/
+
+mv apache-flume-1.9.0-bin.tar.gz ../flume_second_agent/files/
+mv KEYS ../flume_second_agent/files/
